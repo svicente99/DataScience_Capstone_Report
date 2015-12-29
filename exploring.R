4b0e6520dc8bdf29e4dc31114e85f4a6eaf8684e
Sergio Vicente <svicente99@yahoo.com>
2015-12-29 20:15:11 -0200

    Update exploring.R
    Version before Report Submission Assignment (dead line).

--- exploring.R
+++ exploring.R
@@ -4,19 +4,31 @@
 # >> Explore dataset files (samples)
 # author..: Sergio Vicente (@svicente99)
 # data....: 26th Dec. 2015
+# revision: 28th Dec. 2015
 # ---------------------------------------------------------------------
 
 DATASET_DIR = "./Capstone_Dataset/"
 SAMPLE_DIR  = "./Samples/"
-
-SAMPLE_CORPUS = "sample_corpus.txt"
 SAMPLE_CLEAN  = "sample_clean.txt"
 
 cFiles <- c("news","twitter","blogs")
 
-library(tm)
-library(quanteda);
-# http://pnulty.github.io/
+source("plotting.R")
+source("removeFeatures.R")
+
+#-- packages required --#
+
+if(!require("tm")) {
+    install.packages("tm")
+}
+if(!require("quanteda")) {
+    install.packages("quanteda")
+}
+
+library(tm)			# https://cran.r-project.org/web/packages/tm/index.html
+library(quanteda)		# http://pnulty.github.io/
+
+#-- auxiliary functions --#
 
 objRDS <- function(f) {
   return( readRDS(paste0(f, ".rds")) )
@@ -24,15 +36,20 @@ objRDS <- function(f) {
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
+gc(TRUE)	# garbage collector
+
 startTime <- Sys.time()
 
-# generate a txt file with corpora sampling from "Swift Key Dataset", in case of not exists
+# generate 3 txt files with corpora sampling from "Swift Key Dataset", in case of not exists
 
-if(!file.exists(paste0(SAMPLE_DIR,SAMPLE_CORPUS))) 
+if(!file.exists(SAMPLE_DIR)) {
+  dir.create(SAMPLE_DIR)
   lapply(cFiles, 
     function(i) {
-      write.table(objRDS(i), paste0(SAMPLE_DIR,SAMPLE_CORPUS), sep="\t", col.names=FALSE, quote=FALSE, row.names=FALSE, append=TRUE) }
+      write.table(objRDS(i), paste0(SAMPLE_DIR,i,".txt"), sep="\t", col.names=FALSE, quote=FALSE, row.names=FALSE, append=TRUE) }
   )
+}  
+print(paste("Directory",SAMPLE_DIR,"Corpus' created with 3 sample text files."))
 
 # create a primary Corpus using "tm" package that contains all sample texts (as Source) to be cleaned
 # (because cleaning functions for "quanteda' package aren't working fine...)
@@ -42,16 +59,20 @@ if(!file.exists(paste0(SAMPLE_DIR,SAMPLE_CORPUS)))
 if(!file.exists(SAMPLE_CLEAN)) 
 {
   cp <- Corpus(DirSource(SAMPLE_DIR), readerControl = list(language="lat"))
-  cp <- tm_map( tm_map(cp, removeNumbers), removePunctuation )
-  cp <- tm_map(cp, removeWords, stopwords("english"))
+  print("Corpus object's created.")
 
-  # the list of 'stopwords' could be confirmed in next link, according to "quanteda.pdf" package documentation
-  # http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
-  # http://stackoverflow.com/questions/7927367/r-text-file-and-text-mining-how-to-load-data
-
+  #cp <- removeTweetFeatures(cp)
+  cp <- removeOtherFeatures(cp, numbers=TRUE, punctuation=FALSE, spaces=TRUE, stopwords=TRUE)
+  
   # and now, rewrite these data to another file representing sample Corpora already clean up
   dfClean <- data.frame(text=unlist(sapply(cp, `[`, "content")), stringsAsFactors=F) 
-  write.table(dfClean, SAMPLE_CLEAN, sep="\t", col.names=FALSE, quote=FALSE, row.names=FALSE, append=TRUE)
+  write.table(dfClean, SAMPLE_CLEAN, col.names=FALSE, quote=FALSE, row.names=FALSE, append=TRUE)
+  
+  print(paste(SAMPLE_CLEAN,"is generated"))
+
+  # plotting a word cloud graph
+  plot_word_cloud(cp)
+  print("Word cloud graph has been successfully saved.")
   rm(cp)	# (release memory)
 }
 
@@ -64,16 +85,15 @@ summary(mycorp)
 # tokenize the texts from the corpora (one-gram, 2-grams, 3-grams) in data frames in order to
 # show its frequency distribution
 
+print("Tokenizing data...")
 for(i in 1:3) {
   tk <- tokenize(mycorp, ngrams=i, concatenator=',')
-  #colnames(dfg[1]) <- paste0(i,"-word")
   plot_bar_gram( table_tokens(tk), i)
 }
-rm(tk)
-
+rm(tk)		# release memory
 
 # create a dfm, removing stopwords and stemming (not lowering, because it's already done)
-# https://github.com/kbenoit/quanteda/issues/50
+# Ref.: https://github.com/kbenoit/quanteda/issues/50
 
 mydfm <- dfm(mycorp, verbose = TRUE, toLower = FALSE, language="english", stem=TRUE, 
   removeNumbers = TRUE, removePunct = TRUE, removeSeparators = TRUE) 
