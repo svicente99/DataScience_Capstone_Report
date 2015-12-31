fe05ad155cc61c8251e5060a09d2a537fbcb51ee
Sergio Vicente <svicente99@yahoo.com>
2015-12-30 22:39:56 -0200

    Update counting_files.R
    Last correction in "word counting function".

--- counting_files.R
+++ counting_files.R
@@ -15,6 +15,7 @@ DATASET_DIR = "./Capstone_Dataset/"
 
 totSize <- 0
 totLines <- 0
+totWords <- 0
 
 cFiles = c("blogs", "news", "twitter")
 
@@ -38,21 +39,23 @@ linesOf <- function(f) {
   totLines <<- totLines + len
 }
 
+wordsOf <- function(f) {
+  nmFile = paste0("en_US/en_US.",f,".txt")
+  arq <- paste(DATASET_DIR,nmFile,sep="")
+  print(arq)
+  x <- readLines(arq)
+  nWords <- sum(stri_count(x, regex="\\S+"))
+  print(paste(format(nWords,big.mark=" ",decimal.mark="."),"words in text"))  
+  
+  totWords <<- totWords + nWords
+}
+
 lapply(cFiles, function(f) sizeOf(f))
 
 lapply(cFiles, function(f) linesOf(f))
 
+lapply(cFiles, function(f) wordsOf(f))
+
 print(paste("Total size is",sprintf("%5.1f",totSize,"Megabytes")))
 print(paste("Total lines is",format(totLines,big.mark=" ",decimal.mark=".")))
-
-#-- to count Words for 3 files for English LOCALE --#
-
-word_count <- function() {
-  corp <- Corpus(DirSource(paste0(DATASET_DIR,"en_US")))
-  summary(corp)
-  dtm<-DocumentTermMatrix(corp)
-  print(dtm)
-
-  remove(corp)
-  remove(dtm)
-}
+print(paste("Total words is",format(totWords,big.mark=" ",decimal.mark=".")))
